08e2c78a1f5b5a977fc37c0fd732b469345d6cc7
jaunsog <psarmientoj@uninorte.edu.co>
2020-09-09 14:03:24 -0500

    Avance Automático

--- app/src/main/java/com/example/tiorico/MainActivity.java
+++ app/src/main/java/com/example/tiorico/MainActivity.java
@@ -1,194 +1,107 @@
 package com.example.tiorico;
-import android.os.AsyncTask;
-import java.io.DataOutputStream;
+import android.Manifest;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import java.util.Calendar;
+import android.os.Handler;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.net.InetAddress;
+import android.widget.Toast;
+import android.os.AsyncTask;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.Socket;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.os.Looper;
-import android.telephony.SmsManager;
-import android.view.View;
 import android.widget.EditText;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
+import android.content.Context;
+import android.app.AlarmManager;
+import java.net.SocketException;
+import android.location.Location;
+import android.app.PendingIntent;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import static android.widget.Toast.*;
+import android.annotation.SuppressLint;
+import androidx.annotation.RequiresApi;
+import android.location.LocationManager;
+import androidx.core.app.ActivityCompat;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import androidx.core.content.ContextCompat;
 import static android.widget.Toast.makeText;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity<port> extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     EditText IP, puerto;
-
-    private static final int REQUEST_CODE_LOCATION_PERMISSION = 1;
-    int cambiar;
-    int TMensaje  = 0;
-    int port;
-    String Longitude;
-    String Latitude;
+    private Handler hndlr = new Handler();
     String sms;
-    String IPdestino;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         IP = (EditText) findViewById(R.id.IP);
         puerto = (EditText) findViewById(R.id.puerto);
-        ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PackageManager.PERMISSION_GRANTED);
-    }
 
-    public void button(View view) {
-
-        if (ContextCompat.checkSelfPermission(
-                getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION
-        ) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    REQUEST_CODE_LOCATION_PERMISSION
-            );
-        } else {
-            getCurrentLocation();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-            if (requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0) {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getCurrentLocation();
-                } else {
-                    makeText(this, "Permiso denegado (gps)", LENGTH_SHORT).show();
-                }
+        while (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 1000, getIntent(), PendingIntent.FLAG_CANCEL_CURRENT);
+                AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                alarmManager.set(AlarmManager.RTC, System.currentTimeMillis() + 2000, pendingIntent);
+                System.exit(0);
             }
         }
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener location = new getlocation();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 100, location);
+    }
+    public void Stop (View view){
+        hndlr.removeCallbacks(envio);
+    }
+    public void UDPbutton (View view){
+        envio.run();
+    }
 
-            private void getCurrentLocation() {
-                final LocationRequest locationRequest = new LocationRequest();
-                locationRequest.setInterval(10000);
-                locationRequest.setFastestInterval(3000);
-                locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
+    private class getlocation implements LocationListener {
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        @SuppressLint("SetTextI18n")
+        @Override
+        public void onLocationChanged(Location location) {
+            Calendar calendar = Calendar.getInstance();
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyMMddHHmmssZ");
+            String timestamp = simpleDateFormat.format(calendar.getTime());
+            sms=location.getLatitude()+","+location.getLongitude()+","+timestamp;
         }
-        LocationServices.getFusedLocationProviderClient(MainActivity.this)
-                .requestLocationUpdates(locationRequest, new LocationCallback() {
-                    @Override
-                    public void onLocationResult(LocationResult locationResult) {
-                        super.onLocationResult(locationResult);
-                        LocationServices.getFusedLocationProviderClient(MainActivity.this)
-                                .removeLocationUpdates(this);
-                        if (locationResult != null && locationResult.getLocations().size() > 0) {
-                            int latestLocationIndex = locationResult.getLocations().size() - 1;
-                            double latitude = locationResult.getLocations().get(latestLocationIndex).getLatitude();
-                            double longitude = locationResult.getLocations().get(latestLocationIndex).getLongitude();
-                            Latitude = Double.toString(latitude);
-                            Longitude = Double.toString(longitude);
-                            Calendar calendar = Calendar.getInstance();
-                            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MMM-yyyy-hh:mm:ss");
-                            String dateTime = simpleDateFormat.format(calendar.getTime());
-                            sms= "\n La=" + Latitude + "\n Lo= " +Longitude+"\n TS= "+dateTime;
-                            if (!IP.getText().toString().equals("")&&!puerto.getText().toString().equals("")) {
-                            EditText edit = findViewById(R.id.IP);
-                            IPdestino = edit.getText().toString();
-                            port = Integer.parseInt(puerto.getText().toString());
-                            switch (TMensaje) {
-                                case 0:
-                                    TCP TCP = new TCP();
-                                    TCP.execute(sms);
-                                    break;
-                                case 1:
-                                    UDP UDP = new UDP();
-                                    UDP.execute(sms);
-                                    break;
-                            }}
-                        }
-                    }
-                }, Looper.getMainLooper());
     }
-
-    public void TCPbutton(View view) {
-        TMensaje = 0;
-        button(view);
-    }
-
-    public void UDPbutton(View view) {
-        TMensaje = 1;
-        button(view);
-    }
+    private Runnable envio = new Runnable(){
+        @Override
+        public void run() {
+            String msg=sms;
+            UDP UDP1 = new UDP();
+            UDP1.execute(msg);
+            makeText(getApplicationContext(), "Enviado", Toast.LENGTH_LONG).show();
+            hndlr.postDelayed(this, 6000);
+        }
+    };
 
     public class UDP extends AsyncTask<String,Void,Void> {
-
         @Override
         protected Void doInBackground(String... voids) {
             String mensaje = voids[0];
-
-            try{
-                String messageStr = mensaje;
-                int server_port = port;
-                InetAddress local = InetAddress.getByName(IPdestino);
-                int msg_length = messageStr.length();
-                byte[] message = messageStr.getBytes();
-                DatagramSocket s = new DatagramSocket();
-                //
-                DatagramPacket p = new DatagramPacket(message, msg_length, local, server_port);
-                s.send(p);
-            }catch(Exception ex){
-            }
-            return null;
-        }
-    }
-
-    public class TCP extends AsyncTask<String,Void,Void> {
-
-        Socket s;
-        PrintWriter pw;
-        @Override
-        protected Void doInBackground(String... voids) {
-
-            String mensaje = voids[0];
-            try{
-                s = new Socket(IPdestino, port);
-                DataOutputStream dout=new DataOutputStream(s.getOutputStream());
-                dout.writeUTF(mensaje);
-                dout.flush();
-                dout.close();
-                pw = new PrintWriter(s.getOutputStream());
-                pw.write(mensaje);
-                pw.flush();
-                pw.close();
-                s.close();
-            }catch (IOException e)
-            {
+            try {
+                int port = Integer.parseInt(puerto.getText().toString());
+                InetAddress local = InetAddress.getByName(IP.getText().toString());
+                int msg_length = mensaje.length();
+                byte[] mssg = mensaje.getBytes();
+                DatagramSocket su = new DatagramSocket();
+                DatagramPacket p = new DatagramPacket(mssg, msg_length, local, port);
+                su.send(p);
+            } catch (SocketException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-
             return null;
         }
     }
 }
-
 
 
 
